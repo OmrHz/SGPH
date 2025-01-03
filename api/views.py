523a4102d867a00d99257eab1573d0149de621bb
OmrHz <mo_hamza@esi.dz>
2025-01-03 21:54:51 +0100

    add some packages

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from 
+API_KEY = 'your_secure_api_key'
+headers = {
+    'Authorization': f'Bearer {API_KEY}',
+    'Content-Type': 'application/json'
+}
 
-# Create your views here.
+response = requests.get(
+    'https://hospital-dpi-system.com/api/prescriptions/patient/123',
+    headers=headers
+)
